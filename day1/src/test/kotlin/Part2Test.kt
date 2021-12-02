f0e1b6a779225958f65312bd5acd4c098fc4ebd5
xdesign-jamesjones <james.jones@xdesign.com>
2021-12-02 16:27:50 +0000

    day1 problems solved

---
+++ day1/src/test/kotlin/Part2Test.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Part2Test {
+
+    @Test
+    fun calc() {
+        assertEquals(5, Part2().calc("test-depths"))
+    }
+}
